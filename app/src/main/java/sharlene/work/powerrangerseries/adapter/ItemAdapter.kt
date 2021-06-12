22c0af3b21580d3575dff23b3986c4df77dee508
abGhost.cyber <xplendo@gmail.com>
2021-06-12 21:46:23 +0800

    essh

--- app/src/main/java/sharlene/work/powerrangerseries/adapter/ItemAdapter.kt
+++ app/src/main/java/sharlene/work/powerrangerseries/adapter/ItemAdapter.kt
@@ -9,25 +9,27 @@ import androidx.recyclerview.widget.RecyclerView
 import sharlene.work.powerrangerseries.R
 import sharlene.work.powerrangerseries.model.PowerRanger
 
-class ItemAdapter (
+class ItemAdapter(
     private val context: Context,
-    private val  data: List<PowerRanger>
-    ):RecyclerView.Adapter<ItemAdapter.ItemViewHolder>(){
+    private val data: List<PowerRanger>
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
-        class ItemViewHolder(view: View):RecyclerView.ViewHolder(view){
-            val textView:TextView=view.findViewById(R.id.list_item)
+    inner class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.list_item)
+    }
 
-        }
     //size of the data (list)
-    override fun getItemCount()=data.size
+    override fun getItemCount() = data.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val adapterLayout=LayoutInflater.from(parent.context).inflate(R.layout.list,parent,false)
+        val adapterLayout =
+            LayoutInflater.from(parent.context).inflate(R.layout.list, parent, false)
         return ItemViewHolder(adapterLayout)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        val item=data[position]
-        holder.textView.text=context.resources.getString(item.stringResourceId)
+        val item = data[position]
+
+        holder.textView.text = context.resources.getString(item.stringResourceId)
     }
 }
